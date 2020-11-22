a67bf95425b6fbd8ef52cc4dd2c43822c2ca7423
Leonardo A. Perez <xephelsax@gmail.com>
2020-11-22 14:04:38 -0500

    fixed lints and readProperties in gradle

--- app/src/main/java/com/github/ephelsa/mycareer/ui/container/MainActivity.kt
+++ app/src/main/java/com/github/ephelsa/mycareer/ui/container/MainActivity.kt
@@ -2,9 +2,6 @@ package com.github.ephelsa.mycareer.ui.container
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import com.github.ephelsa.mycareer.R
 import com.github.ephelsa.mycareer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
